858194449c96c0e0e7ed2863a5584370b5b886aa
Jessica Perez <1jessicaperez@gmail.com>
2020-07-27 17:57:25 -0400

    Added Employee constructor function, other employee classes, and installed jest

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+//DEPENDENCIES
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+//jest require dependency added
 const jest = require("jest");
 const path = require("path");
 const fs = require("fs");
@@ -10,13 +12,82 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+// READFILE ASYNC DEPENDENCY
+// const readFileAsync = jest.promisify(fs.readFile);
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-const questions = [{}];
 
 // Write code to use inquirer to gather information about the development team members,
+//FUNCTIONS
+//GET USER INPUT ====== inquirer prompt
+function userPrompt(response) {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What type of team member would you like to add?",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+    ])
+    .then(function (reply) {
+      if (reply.role === "Manager" || "Engineer" || "Intern") {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "What is the name of the team member?",
+            name: "name",
+          },
+          {
+            type: "input",
+            message: "What is the id of the team member?",
+            name: "id",
+          },
+          {
+            type: "input",
+            message: "What is the email of the team member?",
+            name: "email",
+          },
+        ]);
+      }
+      if (reply.role === "Manager") {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "What is the office number of the manager?",
+            name: "office",
+          },
+        ]);
+      }
+      if (reply.role === "Engineer") {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "What is the Github user name of the engineer?",
+            name: "github",
+          },
+        ]);
+      } else if (reply.role === "Intern") {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "What is the school attended by the intern?",
+            name: "school",
+          },
+        ]);
+      }
+    });
+}
+async function init() {
+  const response = await inquirer.prompt(questions);
+  console.log(response);
+  //if answer to first question is "Manager" then go to questions Manager
+}
+init();
+// let newUserData = generateHTML(response);
+// writeFileAsync("user-data.html", newUserData);
 
 // and to create objects for each team member (using the correct classes as blueprints!)
 
